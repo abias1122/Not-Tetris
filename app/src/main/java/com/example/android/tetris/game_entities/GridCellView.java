2d61278a661d0322a519e102a1dbda6d9768adab
Anthony Bias <abias1122@gmail.com>
2017-08-07 13:45:46 -0400

    Create GridCellUnit class
    Will be used to make up both gameboard and tetronimoes.

---
+++ app/src/main/java/com/example/android/tetris/game_entities/GridCellView.java
@@ -0,0 +1,10 @@
+package com.example.android.tetris.game_entities;
+
+import android.widget.ImageView;
+
+/**
+ * Created by Anthony on 8/7/2017.
+ */
+
+public class GridCellView extends ImageView {
+}
